30a3d251e68232622014cc6849066690468ca660
kannanpalaniappan-repo <kannanpalaniappan2001@gmail.com>
2025-04-09 08:39:55 +0530

    Response Entity Class

--- src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -1,7 +1,9 @@
 package com.ecommerce.project.service;
 
 import com.ecommerce.project.model.Category;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,10 +30,7 @@ public class CategoryServiceImpl implements CategoryService{
 
         Category category = categories.stream().
                 filter(c->c.getCategoryId().equals(categoryId))
-                .findFirst().orElse(null);
-
-        if(category==null)
-            return "category not found";
+                .findFirst().orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND,"Resource Not Found"));
 
         categories.remove(category);
 

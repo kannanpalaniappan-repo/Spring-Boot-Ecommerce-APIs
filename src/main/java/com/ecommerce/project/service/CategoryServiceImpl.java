00603eab505b8e298f6030b990848d705e5bdd2b
kannanpalaniappan-repo <kannanpalaniappan2001@gmail.com>
2025-04-23 21:37:11 +0530

    Validation for No Categories Present

--- src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -22,8 +22,10 @@ public class CategoryServiceImpl implements CategoryService{
 
     @Override
     public List<Category> getAllCategories() {
-
-        return categoryRepository.findAll();
+        List<Category> categories = categoryRepository.findAll();
+        if(categories.isEmpty())
+            throw new APIException("No Category Created Till Now...");
+        return categories;
     }
 
     @Override

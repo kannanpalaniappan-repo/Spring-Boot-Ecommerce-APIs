f43e22be554ccaaeec498454c75bab4bb9cad78c
kannanpalaniappan-repo <kannanpalaniappan2001@gmail.com>
2025-04-27 13:42:14 +0530

    Implementing DTO Pattern

--- src/main/java/com/ecommerce/project/service/CategoryService.java
+++ src/main/java/com/ecommerce/project/service/CategoryService.java
@@ -1,11 +1,12 @@
 package com.ecommerce.project.service;
 
 import com.ecommerce.project.model.Category;
+import com.ecommerce.project.payload.CategoryResponse;
 
 import java.util.List;
 
 public interface CategoryService {
-    List<Category> getAllCategories();
+    CategoryResponse getAllCategories();
     String createCategory(Category category);
 
     String deleteCategory(Long categoryId);
